a45539ad81e8b5f3b0215d010f21b6bab5288d3c
Manuel Ferrazzani <manu.ferra.mf@gmail.com>
2022-12-11 11:53:58 +0100

    add some features and solved some bugs
    now you can download animes with their episodes number. See README.md for more

--- animedl.py
+++ animedl.py
@@ -1,3 +1,4 @@
+from ast import arg
 import urllib.request
 from webbrowser import open as wbopen
 from urllib.error import HTTPError as HTTPError
@@ -5,9 +6,17 @@ from time import sleep
 import os
 import argparse
 import sqlite3
+import ssl
+
+def findAnimeLinkNameLength(animeName, index):
+	i=0
+	while True:
+		if(animeName[index+i].isnumeric() == False):
+			return i
+		i+=1
 
 def downloadIsNotCompleted():
-	for fname in os.listdir("C:\\Users\\Utente\\Downloads"):
+	for fname in os.listdir("C:\\Users\\manuf\\Downloads"):
 		if fname.endswith(".part") or fname.endswith(".crdownload"):
 			return 1
 	return 0
@@ -22,13 +31,24 @@ def animeInsert(name):
 	cur.execute("INSERT INTO anime (name, num, link) VALUES (?, ?, ?)", (name, num, link))
 	db.commit()
 
-db=sqlite3.connect("C:\\Users\\Utente\\anime.db")
+db=sqlite3.connect("C:\\Users\\manuf\\anime.db")
 cur=db.cursor()
 
 parser=argparse.ArgumentParser()
 parser.add_argument('animeName', type=str, help='Nome dell\'anime')
 parser.add_argument('epNumber', type=int, help='Numero di episodi')
 args=parser.parse_args()
+
+flagDownloadEpNum = False
+flagUpdateEpNum = False
+if(args.animeName[0] == '.'):
+    flagDownloadEpNum = True
+    args.animeName = str(args.animeName[1:])
+
+elif(args.animeName[0] == '!'):
+    flagDownloadEpNum = True
+    flagUpdateEpNum = True
+    args.animeName = str(args.animeName[1:])
 
 animeName=args.animeName
 numEp=args.epNumber
@@ -45,27 +65,54 @@ except IndexError:
 	quit()
 
 epNum=findEpNum(animeName)
-desEpNum=epNum+numEp
-index=string.find("Ep_")+3
-while(epNum<desEpNum):
-	epNum+=1
-	epNumStr=str(epNum)
-	if(len(epNumStr)==1):
-		epNumStr='0'+epNumStr
-	string=string[:index] + epNumStr + string[-12:]
-	try:
-		urllib.request.urlopen(string)
-		wbopen(string)
-		sleep(5)
-		cur.execute("UPDATE anime SET num=? WHERE name=?", (epNum, animeName))
-		db.commit()
-	except HTTPError:
-		print("Episodio non esistente o link anime modificato")
-		pass
-	else:
-		while(downloadIsNotCompleted()):
+if flagDownloadEpNum:
+    epNum = numEp
+    index=string.find("Ep_")+3
+    animeLinkNameLenght=findAnimeLinkNameLength(string, index) 
+    epNumStr=str(epNum)
+    if(len(epNumStr)==1):
+        epNumStr='0'+epNumStr
+    string=string[:index] + epNumStr + string[index+animeLinkNameLenght:]
+    try:
+        context = ssl._create_unverified_context()
+        urllib.request.urlopen(string, context=context)
+        wbopen(string)
+        sleep(5)
+        if flagUpdateEpNum:
+            cur.execute("UPDATE anime SET num=? WHERE name=?", (epNum, animeName))
+            db.commit()
+    except HTTPError:
+        print("Episodio non esistente o link anime modificato")
+        pass
+    else:
+        while(downloadIsNotCompleted()):
+            pass
+        print(str(animeName)+"-"+str(epNum), "scaricato correttamente")
+
+else:
+	desEpNum=epNum+numEp
+	index=string.find("Ep_")+3
+	animeLinkNameLenght=findAnimeLinkNameLength(string, index)
+	while(epNum<desEpNum):
+		epNum+=1
+		epNumStr=str(epNum)
+		if(len(epNumStr)==1):
+			epNumStr='0'+epNumStr
+		string=string[:index] + epNumStr + string[index+animeLinkNameLenght:]
+		try:
+			context = ssl._create_unverified_context()
+			urllib.request.urlopen(string, context=context)
+			wbopen(string)
+			sleep(5)
+			cur.execute("UPDATE anime SET num=? WHERE name=?", (epNum, animeName))
+			db.commit()
+		except HTTPError:
+			print("Episodio non esistente o link anime modificato")
 			pass
-		print(str(animeName)+"-"+str(epNum), "scaricato correttamente")
+		else:
+			while(downloadIsNotCompleted()):
+				pass
+			print(str(animeName)+"-"+str(epNum), "scaricato correttamente")
 
 if db:
 	db.close()
